c7e8212f34b8f28cc6cc231bf5837297b2134335
Nikhil Bhardwaj <nikhil.bhardwaj@mobileprogramming.com>
2024-03-05 14:32:50 +0530

    fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,8 +79,8 @@ export default function PaymentScreen() {
         },
       })
       .then((response) => {
-        console.log(response.data);
-        return response.data
+        console.log(response.data.data);
+        return response.data.data
       })
       .catch((error) => {
         console.error(error);
